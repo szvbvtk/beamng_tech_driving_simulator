e722a8b43c3797780b4ab7cf988d115fcc39488a
Krystian Szabat <krystian.szabat@outlook.com>
2024-03-20 12:13:23 +0100

    test odpalenia gry przez strone

--- flask_client_old/receiver.py
+++ flask_client_old/receiver.py
@@ -1,6 +1,9 @@
 import socket
+from beamngpy import BeamNGpy, Scenario, Vehicle
+import json
+import ast
 
-tcp_ip = '127.0.0.1'
+tcp_ip = "127.0.0.1"
 tcp_port = 4917
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,7 +15,35 @@ conn, addr = s.accept()
 
 while True:
     data = conn.recv(1024)
+    if data:
+        print(data)
     if not data:
         break
-    print("received data:", data)
-    conn.send(data) 
+    # print("received data:", data)
+    py_obj = ast.literal_eval(data.decode("utf-8"))
+    command = py_obj['command']
+
+    # print(field_value)
+    if command == "start-scenario":
+        beamng = BeamNGpy(
+            host="localhost",
+            port=64256,
+            home="D:\BeamNG.tech\BeamNG.tech.v0.31.2.0",
+            user="D:\BeamNG.tech\BeamNG.tech.v0.31.2.0\Bin64",
+        )
+        beamng.open()
+
+        scenario = Scenario("west_coast_usa", "example")
+        # Create an ETK800 with the licence plate 'PYTHON'
+        vehicle = Vehicle("ego_vehicle", model="etk800", license="PYTHON")
+        # Add it to our scenario at this position and rotation
+        scenario.add_vehicle(
+            vehicle, pos=(-717, 101, 118), rot_quat=(0, 0, 0.3826834, 0.9238795)
+        )
+
+        scenario.make(beamng)
+
+        # Load and start our scenario
+        beamng.scenario.load(scenario)
+        beamng.scenario.start()
+    conn.send(data)
